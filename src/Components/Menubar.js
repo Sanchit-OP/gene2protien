89968f27be4e4070653713e1974a84d1c4dc7100
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-08-22 22:24:07 +0530

    changes

--- src/Components/Menubar.js
+++ src/Components/Menubar.js
@@ -1,15 +1,17 @@
 import './Menubar.css';
 import MenuItem from './MenuItem';
-
+import Logo from './Logo';
 function Menubar(){
     return(
         <div className='menubar'>
+            <Logo />
             <MenuItem name='products'/>
-            <MenuItem name='products'/>
-            <MenuItem name='products'/>
-            <MenuItem name='products'/>
-            <MenuItem name='products'/>
-            <MenuItem name='products'/>
+            <MenuItem name='resource'/>
+            <MenuItem name='carrers'/>
+            <MenuItem name='menu'/>
+            <MenuItem name='about'/>
+            <MenuItem name='services'/>
+            <MenuItem name='contact'/>
         </div>
     );
 }
