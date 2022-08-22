abe1172589fd1300d669fa273bd37fe82b97119a
Praxorocks <75744656+Praxorocks@users.noreply.github.com>
2022-08-22 20:19:46 +0530

    lawda

---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+import './Logo.css';
+
+function Logo(){
+    return(
+        <div className="logo">
+            {/* <img className="logo" src="../Ui/g2p_logo.svg"></img> */}
+        </div>
+    )
+}
+
+export default Logo
