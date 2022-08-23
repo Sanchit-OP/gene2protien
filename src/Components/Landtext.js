4c2ab909cb228ba87a57898e05814f1c87f605e0
Praxorocks <75744656+Praxorocks@users.noreply.github.com>
2022-08-23 11:29:12 +0530

    backg

--- src/Components/Landtext.js
+++ src/Components/Landtext.js
@@ -6,7 +6,7 @@ function Landtext(){
         <div class="banner-container">
             <h1 className="land-title d-blue">Lorem Ipsum lawda lasun</h1>
             <h5 className="under-title white">Lorem Ipsum bada lawda</h5>
-            <img id='Bottle-style' src={Bottle} />
+            {/* <img id='Bottle-style' src={Bottle} /> */}
             <div>
                 <button className="ban-button white">LEARN MORE</button>
             </div>
