db35604545fda9b54ac4c778ad0afbbd1d893685
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-10-21 20:46:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import PRODUCTS from './pages/products';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/products' element={<PRODUCTS />} />
     </Routes>
   );
 }
