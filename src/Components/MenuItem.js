a1bed5a27121f11a2447dbe6ee19183c0c6cb277
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-20 13:52:49 +0530

    tailwind

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -3,7 +3,7 @@ import "./MenuItem.css";
 function MenuItem(props) {
   return( 
     <div className="item">
-        <div className="itemName"> {props.name.toUpperCase()} </div>
+        <div className="itemName" > {props.name.toUpperCase()} </div>
     </div>
   );
 }
