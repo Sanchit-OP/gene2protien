5eaa62ba6d3c48c322f0eb906983618ada50c7f7
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-21 15:12:27 +0530

    dfs

--- src/Components/AboutVideo.js
+++ src/Components/AboutVideo.js
@@ -1,10 +1,10 @@
-
+import interview from '../Ui/interview.mp4';
 function AboutVideo() {
     return (
         <div className='mx-10 shadow-2xl shadow-zinc-900 ' style={{ borderRadius: 25 }}>
-            {/* <video width="800" height="650" autoPlay controls style={{ borderRadius: 25 }}>
+            <video width="800" height="650" autoPlay controls style={{ borderRadius: 25 }}>
                 <source src={interview} type="video/mp4" />
-            </video> */}
+            </video>
         </div>
     )
 }
