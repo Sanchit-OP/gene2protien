f9a911e0fba84690f7921e5f190d23299a17fba7
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-21 11:55:32 +0530

    update

--- src/pages/pdf.js
+++ src/pages/pdf.js
@@ -4,6 +4,6 @@ export default function pdf(){
     return(
         <iframe
             className="w-screen h-screen"
-         src="https://drive.google.com/file/d/1qYyam3eGtyl568V_h3inLb8mJWir94kU/preview" width="640" height="480" allow="autoplay"></iframe>
+        src="https://drive.google.com/file/d/1qYyam3eGtyl568V_h3inLb8mJWir94kU/preview" width="640" height="480" allow="autoplay"></iframe>
     );
 }
