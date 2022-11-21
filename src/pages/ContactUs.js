323f74f25ee9151520c034558f9acca190371df8
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-21 13:08:38 +0530

    mail

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Contact from '../Components/Contact';
+
+export default function ContactUs(){
+    
+    return (
+        <div>
+        <Contact />
+        {/* <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form> */}
+      </div>
+    );
+}
