e54f9fbf697e2e73c27e9e295928f88db285f5b3
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-22 16:42:10 +0530

    logoswabhiman

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -2,7 +2,7 @@
 import LogoG from '../Ui/g20.png';
 function Logo(){
     return(
-        <div className="w-56 h-auto mt-3 ml-6 mr-0">
+        <div className=" hidden md:block max-w-[17rem] opacity-100 duration-[600ms] m-3 hover:opacity-80">
             <img className="logo" src={LogoG} />
         </div>
     )
