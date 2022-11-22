2e86ca96fb288da84e6929c7faf732237a23772a
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-11-22 15:39:06 +0530

    dfs

--- src/Components/Lion.js
+++ src/Components/Lion.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Lionimg from '../Ui/makeInIndia.svg';
+import Lionimg from "../Ui/makeInIndia.svg";
 
-export default function Lion(){
-    return(
-            <div className="hidden md:block w-[50%]  ml-[15em] my-10">
-                <img src={Lionimg} alt="none" />
+export default function Lion() {
+  return (
+            <div className="hidden md:block w-[50%] my-10 max-w-screen-lg mx-auto pb-10 flex">
+                <img className="mx-auto" src={Lionimg} alt="none" />
             </div>
-    );
+  );
 }
