e61ef86ecec2c643b4948d5c11467514e891326c
Sanchit-OP <75922887+Sanchit-OP@users.noreply.github.com>
2022-12-13 14:54:37 +0530

    Update ItemBar.js

--- src/Components/ItemBar.js
+++ src/Components/ItemBar.js
@@ -2,7 +2,7 @@ import React from "react";
 function ItemBar(props) {
     return (
         <div className="w-4/5 h-[10%] px-5 pt-5 pb-10 duration-100 grid grid-flow-col grid-cols-10" style={{ backgroundColor: props.color }}>
-
+            {/* lot of changes */}
             <h1 className="col-span-2" >{props.catno}</h1>
             <h1 className=" col-span-5" >{props.title}</h1>
             <h1 className="font-[poppins] col-span-1" >{props.price}</h1>
